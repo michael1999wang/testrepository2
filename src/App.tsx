a295a28078273d85c97a0db7fabf6589bb87b51a
Michael Wang <michael1999wang@gmail.com>
2023-02-07 20:26:22 -0500

    changing text

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>this was edited again</p>
+				<p>tomato</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
